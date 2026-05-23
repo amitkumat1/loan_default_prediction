1bf26526c42ba29a57898d126c5db3a2de30c1bd
Amit Kumar Singh <amitkumars342@gmail.com>
2026-05-23 16:01:57 +0530

    Logging and Exception added

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from typing import List
 
 HYPHEN_E_DOT = '-e .'
 def get_requirements(file_path:str) -> list[str]:
-    with open(file_path, 'r') as f:
-        f.readlines()
-        requirements = [req.replace('\n', '') for req in f.readlines()]
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
     return requirements
